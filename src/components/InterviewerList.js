41cc0664551257b28d46ef306d9cc5f46604b13f
Caseyann <goore_44@hotmail.ca>
2022-01-19 00:47:02 -0500

    implement InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,7 +4,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
 
-  const mappedList = props.interviewers.map(interviewer => {
+  const interviewers = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
         key={interviewer.id}
@@ -19,7 +19,7 @@ export default function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light"></h4>
-      <ul className="interviewers__list">{mappedList}</ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 } 
