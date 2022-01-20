9d1bc902aec7d4004a56ba2179705cff61fadd30
Caseyann <goore_44@hotmail.ca>
2022-01-19 22:42:35 -0500

    implement confirm, error, show, and status components and stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,22 +1,16 @@
 import React from "react";
-import "./styles.scss"
+import "./styles.scss";
+
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 export default function Appointment(props) {
-
-  return( 
+  return (
     <article className="appointment">
-         {
-        props.time &&
-        <fragment>
-          Appointment at {props.time}
-        </fragment>
-      }
-      {
-        !props.time &&
-        <fragment>
-          No Appointments
-        </fragment>
-      }
+      <Header time={props.time} />
+      {props.time && <fragment>Appointment at {props.time}</fragment>}
+      {!props.time && <fragment>No Appointments</fragment>}
     </article>
   );
-};
+}
